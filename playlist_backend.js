8ef065e8bb1cdea29b621609da90c5bcf96a0862
Ipsaeju <sanddepanda@gmail.com>
2019-01-31 11:40:12 -0800

    Created playlist_backend.js
    Currently only has init of functions and comments on what they will do

---
+++ playlist_backend.js
@@ -0,0 +1,19 @@
+function createPlaylist() {
+    //This is where the playlist is given a title and added to playlist.json
+}
+
+function addSongToPlaylist() {
+    //This is where you can add songs to the playlist
+}
+
+function deleteSongOnPlaylist() {
+    //This is where you can delete songs from a playlist
+}
+
+function deletePlaylist() {
+    //This is where the playlist is deleted and removed from playlist.json
+}
+
+function displayPlaylist() {
+    //This will show the playlist & its contents
+}
