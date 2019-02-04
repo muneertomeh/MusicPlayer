50712158efc189fc8bfded7caeb39d59653cb042
Lucas D'Avila <07luc@live.com>
2019-02-04 09:09:05 -0800

    Added Music Player
    Updated the search functions to perform search on music.json file and added music player to play music files

--- service/dashboard.js
+++ service/dashboard.js
@@ -18,3 +18,85 @@ window.addEventListener('beforeunload', ()=>{
     globalShortcut.unregister('F5', reload);
     globalShortcut.unregister('CommandOrControl+R', reload);
 })
+
+
+function searchForSongs() {
+    let searchType = null;
+
+    if (document.getElementById('search_song_title').checked == true) {
+        searchType = document.getElementById('search_song_title').value;
+    }
+    if (document.getElementById('search_song_artist').checked == true) {
+        searchType = document.getElementById('search_song_artist').value;
+    }
+    if (document.getElementById('search_song_release').checked == true) {
+        searchType = document.getElementById('search_song_release').value;
+    }
+    let searchField = document.getElementById('search_song').value;
+
+    //console.log("search Type: " + searchType + "What's searched: " + searchField);
+    if(searchType == null || searchField == '' || searchField == null) {
+        alert('Please fill in all required fields');
+    } else {
+        fs.readFile(__dirname + '/../data/music.json', (err, rawData) => {
+            if(err) {
+                console.log(err);
+            } else {
+                let data = JSON.parse(rawData);
+                let list = document.querySelector('ul');
+                list.innerHTML = '';
+                let id = 'song_';
+                let idNum = 1;
+                data.forEach(element => {
+                    if(searchType == 'song') {  
+                        if(searchField == element[searchType]['title']) {
+                            listItem = document.createElement('li');
+                            listItem.textContent = searchField + ' by: ' + element['artist']['name'];
+                            listItem.className = 'song_info';
+                            listItem.innerHTML += '<button type="submit" class="play_song_button" id="play_song_button" onclick="playSong()">Play Song</button>';
+                            id += idNum.toString();
+                            listItem.id = id;
+                            listItem.datatype = element['song']['file'];
+                            list.appendChild(listItem);
+                            idNum += 1;
+                        }
+                    }
+                    else if(searchType == 'artist') {
+                        if(searchField == element[searchType]['name']) {
+                            listItem = document.createElement('li');
+                            listItem.textContent = element['song']['title'] + ' by: ' + searchField;
+                            listItem.className = 'song_info';
+                            listItem.innerHTML += '<button type="submit" class="play_song_button" id="play_song_button" onclick="playSong()">Play Song</button>';
+                            id += idNum.toString();
+                            listItem.id = id;
+                            listItem.datatype = element['song']['file'];
+                            list.appendChild(listItem);
+                            idNum += 1;
+                        }
+                    }
+                    else if(searchType == 'release') {
+                        if(searchField == element[searchType]['name']) {
+                            listItem = document.createElement('li');
+                            listItem.textContent = element['song']['title'] + ' by: ' + element['artist']['name'];
+                            listItem.className = 'song_info';
+                            listItem.innerHTML += '<button type="submit" class="play_song_button" id="play_song_button" onclick="playSong()">Play Song</button>';
+                            id += idNum.toString();
+                            listItem.id = id;
+                            listItem.datatype = element['song']['file'];
+                            list.appendChild(listItem);
+                            idNum += 1;
+                        }
+                    }
+                })
+            }
+        })
+    }
+}
+
+function playSong(id) {
+    let musicPlayer = document.getElementsByClassName('player_buttons');
+    musicPlayer.innerHTML = '';
+
+    let currentSong = document.getElementById('music');
+    currentSong.play();
+}
