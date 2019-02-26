4aca156ee517623fa9c0aba99f68bddaee310ff9
Lucas D'Avila <07luc@live.com>
2019-02-25 21:28:39 -0800

    Login Service Class + methods
    Created user object and method to get list of users from JSON file.  Began writing function to log user in.

--- Server/LoginServices.java
+++ Server/LoginServices.java
@@ -1,1 +1,40 @@
-import java.util.*;
+import java.io.*;
+import java.util.List;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+public class LoginServices {
+	
+	private User user;
+	private String userFPath;
+	
+	public LoginServices() {
+		user = new User();
+		userFPath = "/../client/data/users.json";
+	}
+	
+	public List<String> getUsers() {
+		try {
+			BufferedReader bufReader = new BufferedReader(new FileReader(userFPath));
+			Type jsonListType = new TypeToken<List<String>>() {}.getType();
+            List<String> userList = new Gson().fromJson(bufReader, jsonListType);
+            
+            return userList;
+		}catch(FileNotFoundException e) {
+            System.out.println("Error: " + e);
+        }
+	}
+	
+	public boolean Login(String userName, String password) {
+		boolean successfulLogin = false;
+		List<String> userList = getUsers();
+		if(userName.equals(null) || password.equals(null) || userName == "" || password == "") {
+			return successfulLogin;
+		}
+		
+		return successfulLogin;
+	}
+	
+	
+
+}
