3646c5454b2970f67296495c9ae6f27f64fd5f6f
ChrisMeyer7088 <chrismeyer175@gmail.com>
2019-02-28 11:14:46 -0800

    Committing working files

--- Client/service/registration.js
+++ Client/service/registration.js
@@ -14,56 +14,7 @@ function register() {
     } else if(password !== cpassword) {
         alert('Passwords must match');
     } else {
-        fs.readFile(__dirname + '/../data/users.json', (err, data) => {
-            if(err) console.log(err);
-            else {
-                let rawData = JSON.parse(data);
-                let isUniqueUserName = true;
-                rawData['users'].forEach(element => {
-                    if(userName == element['UserName']){
-                        isUniqueUserName = false; 
-                    }
-                });
-
-                if(!isUniqueUserName) {
-                    alert('UserName has already been taken');
-                }
-                else {
-                    rawData['users'].push({ 
-                        'UserName': userName,
-                        'Password': password
-                    });
-                    let json = JSON.stringify(rawData);
-                    fs.writeFile(__dirname + '/../data/users.json', json, (err) => {
-                        if(err) console.log(err);
-
-                    });
-                    let playlistInit = [];
-                    fs.readFile(__dirname + '/../data/playlist.json', (err, data) => {
-                        if(err) console.log(err);
-                        else{
-                            let pData = JSON.parse(data);
-                            pData["UserPlaylists"].push({
-                                "UserName": userName,
-                                "Playlists": playlistInit
-                            });
-                            let json = JSON.stringify(pData);
-                            fs.writeFile(__dirname + '/../data/playlist.json', json, (err) => {
-                                if(err) console.log(err);
-                                else{
-                                    let win = remote.getCurrentWindow();
-                                        win.loadURL(url.format({
-                                            pathname: path.join(__dirname, '/../view/login.html'),
-                                            protocol: 'file',
-                                            slashes: true
-                                        }));
-                                }
-                            });
-                        }
-                    });
-                }
-            }
-        });
         
     }
+
 }
